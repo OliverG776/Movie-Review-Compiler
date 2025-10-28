06d901d1a3fa3034e7143851f4efb01d49d410b3
OliverG776 <olivergrushka475@gmail.com>
2025-10-28 11:06:05 -0400

    Test commit

---
+++ UserInterface.py
@@ -0,0 +1,30 @@
+import streamlit as st
+#Run Command:
+#streamlit run C:\Users\olive_lxjq7tp\PycharmProjects\MovieReview\UserInterface.py
+
+col1, col2, col3 = st.columns([1, 3, 1])
+searchActor = False
+
+with col2:
+    st.title("Search for Reviews:")
+
+
+#st.write("Hello, Streamlit!")
+
+search = st.text_input("Search here!")
+if search:
+    st.write(search)
+
+search_toggle = st.radio(
+    "Search by:", ("Film Title", "Actor"), horizontal=True
+)
+
+if search_toggle == "Film Title":
+    searchActor = False
+else:
+    searchActor = True
+
+with col3:
+    if st.button("Top Films"):
+        print("Redirecting to top films link")
+        st.switch_page("pages/TopFilms.py")
