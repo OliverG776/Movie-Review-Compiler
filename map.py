56ad89ad857da8360c609c8d0e31c6fe4f831a07
parkercolby <colbyparker1905@gmail.com>
2025-10-30 13:24:44 -0400

    Updated Map - Insertion Functions
    I added the basic insertion, probing in case of collision, hashing, and resizing functions. **Needs Deletion Implementation**

--- map.py
+++ map.py
@@ -2,13 +2,17 @@ class Map:
     def __init__(self, dataType1, dataType2):
         self.dataType1 = dataType1
         self.dataType2 = dataType2
-        self.mapContainer = [0] * 10
+        self.mapContainer = [None] * 10
+        self.load_factor = 0.0
+        self.buckets = 0
 
-    def __getitem__(self, item):
-        return self.dataType1[item]
+    def __getitem__(self, key):
+        return self.dataType1[key]
 
     def __setitem__(self, key, value):
+
         self.dataType1[key] = value
+
 
     def __delitem__(self, key):
         del self.dataType1[key]
@@ -16,11 +20,48 @@ class Map:
     def __eq__(self, other):
         return self.dataType1 == other.dataType1 and self.dataType2 == other.dataType2
 
-    def __hash__(self):
-        return hash(self.dataType1)
+    def __hash__(self, key):
+        pow = 0
+        sum = 0
+        for i in str(key):
+            sum += ord(i)*27**pow
+            pow+=1
+        return sum%len(self.mapContainer)
 
-    def resize(self, width, height):
-        return 0
+    def probe(self, index):
+        pow = 1;
+        newIndex = index
+        while not(self.mapContainer[newIndex] is None) and (self.mapContainer[newIndex][0] != "<deletedBucket>"):
+            newIndex = (index + pow**2) % len(self.mapContainer)
+            pow += 1
+        return newIndex;
+
+    def insert(self, key, value):
+        keyHash = self.__hash__(key)
+        if self.mapContainer[keyHash] is None:
+            self.mapContainer[keyHash] = (key,value)
+            self.buckets += 1
+        elif self.mapContainer[keyHash] and (self.mapContainer[keyHash][0] == "<deletedBucket>"):
+            self.mapContainer[keyHash] = (key,value)
+            self.buckets += 1
+        else:
+            keyHash = self.probe(keyHash)
+            self.mapContainer[keyHash] = (key,value)
+            self.buckets+=1
+
+        self.load_factor = self.buckets / len(self.mapContainer)
+        if self.load_factor > 0.7:
+            self.resize()
+
+    def resize(self):
+        oldMap = self.mapContainer
+        newSize = len(self.mapContainer)*2
+        self.mapContainer = [None]*newSize
+        self.buckets = 0
+        for i in oldMap:
+            if i and (i[0] != "<deletedBucket>"):
+                self.insert(i[0],i[1])
+        self.load_factor = self.buckets/len(self.mapContainer)
 
     def __contains__(self, item):
         return item in self.dataType1
