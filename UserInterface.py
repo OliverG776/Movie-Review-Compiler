81ed930347f9f92144ee6dda041da6593c32657a
OliverG776 <olivergrushka475@gmail.com>
2025-11-03 09:52:01 -0500

    -Genres appear when searching for a title, no longer need film year to find a specific title (Outside of the case where we have extra parenthesis, I'll swap it out later)

--- UserInterface.py
+++ UserInterface.py
@@ -1,33 +1,44 @@
+#Imports
 import streamlit as st
+
 #Run Command:
-#streamlit run C:\Users\olive_lxjq7tp\PycharmProjects\MovieReview\UserInterface.py
 #streamlit run UserInterface.py
 
+#Variables:
+
+#Defines columns, essentially partitions the page so we can set assets to certain parts of the page
 col1, col2, col3 = st.columns([1, 3, 1])
-searchActor = False
+searchGenre = False
+#Sets user's default search to ""
 if 'search_key' not in st.session_state:
     st.session_state['search_key'] = ""
 
+#UI Setup
 with col2:
     st.title("Search for Reviews:")
 
 search = st.text_input("Search here!")
 if search:
+    #Changes 'search_key' to str(search), then performs search
     st.session_state['search_key'] = str(search)
-    st.write(search)
+    #st.write(search)
     st.switch_page("pages/Search.py")
 
+#Allows the user to choose what category they want to sort by
 search_toggle = st.radio(
-    "Search by:", ("Film Title", "Actor"), horizontal=True
+    "Search by:", ("Film Title", "Genre"), horizontal=True
 )
 
-st.write("search is", st.session_state['search_key'])
+#Testing
+#st.write("search is", st.session_state['search_key'])
 
+#Switches what user is searching by
 if search_toggle == "Film Title":
-    searchActor = False
+    searchGenre = False
 else:
-    searchActor = True
+    searchGenre = True
 
+#Top films page
 with col3:
     if st.button("Top Films"):
         print("Redirecting to top films link")
--- pages/Search.py
+++ pages/Search.py
@@ -1,10 +1,12 @@
+#Imports
 import streamlit as st
 import os
 import csv
 
+#Initializing streamlit variables
 col1, col2, col3 = st.columns([1, 3, 1])
 script_directory = os.path.dirname(__file__)
-file_directory = os.path.join(script_directory, "movies.csv")
+file_directory = os.path.join(script_directory,"movies.csv")
 
 
 with col2:
@@ -15,10 +17,14 @@ with open(file_directory, newline="", encoding="utf-8") as csvfile:
     next(reader)
     i = 1
     for row in reader:
-        if row[1] == st.session_state['search_key']:
-            st.write(row[1])
+        title = row[1].split("(")
+        title[0] = title[0][:-1]
+        if title[0].lower() == st.session_state['search_key'].lower():
+            st.write(title[0])
+            st.write("Genres: " + row[2])
+            #Need to check map here to pull out ratings/potentially switch genres to come from map
 
-#ISSUE: Need film year included as well, we can probably fix it just by identifying when we hit the first (
+#ISSUE: Film year included as well, we can probably fix it just by identifying when we hit the first (
 
 with col3:
     if st.button("Home"):
