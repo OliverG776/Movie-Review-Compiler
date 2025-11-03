349016e2d1ff2ddf82ba96caf1329cd0e06b6713
parkercolby <colbyparker1905@gmail.com>
2025-11-03 17:57:48 -0500

    Insert Function Complete
    Needs Search to be Implemented.

--- map.py
+++ map.py
@@ -1,24 +1,12 @@
 class Map:
-    def __init__(self, dataType1, dataType2):
-        self.dataType1 = dataType1
-        self.dataType2 = dataType2
+    def __init__(self):
         self.mapContainer = [None] * 10
         self.load_factor = 0.0
         self.buckets = 0
 
-    def __getitem__(self, key):
-        return self.dataType1[key]
-
-    def __setitem__(self, key, value):
-
-        self.dataType1[key] = value
-
-
-    def __delitem__(self, key):
-        del self.dataType1[key]
-
-    def __eq__(self, other):
-        return self.dataType1 == other.dataType1 and self.dataType2 == other.dataType2
+    def __setitem__(self, title, values):
+        genres, rating = values
+        self.insert(title, genres, rating)
 
     def __hash__(self, key):
         pow = 0
@@ -31,22 +19,19 @@ class Map:
     def probe(self, index):
         pow = 1;
         newIndex = index
-        while not(self.mapContainer[newIndex] is None) and (self.mapContainer[newIndex][0] != "<deletedBucket>"):
+        while not(self.mapContainer[newIndex] is None):
             newIndex = (index + pow**2) % len(self.mapContainer)
             pow += 1
         return newIndex;
 
-    def insert(self, key, value):
-        keyHash = self.__hash__(key)
+    def insert(self, title, genres, rating):
+        keyHash = self.__hash__(title)
         if self.mapContainer[keyHash] is None:
-            self.mapContainer[keyHash] = (key,value)
-            self.buckets += 1
-        elif self.mapContainer[keyHash] and (self.mapContainer[keyHash][0] == "<deletedBucket>"):
-            self.mapContainer[keyHash] = (key,value)
+            self.mapContainer[keyHash] = (title,genres,rating)
             self.buckets += 1
         else:
             keyHash = self.probe(keyHash)
-            self.mapContainer[keyHash] = (key,value)
+            self.mapContainer[keyHash] = (title,genres,rating)
             self.buckets+=1
 
         self.load_factor = self.buckets / len(self.mapContainer)
@@ -59,8 +44,8 @@ class Map:
         self.mapContainer = [None]*newSize
         self.buckets = 0
         for i in oldMap:
-            if i and (i[0] != "<deletedBucket>"):
-                self.insert(i[0],i[1])
+            if i:
+                self.insert(i[0],i[1], i[2])
         self.load_factor = self.buckets/len(self.mapContainer)
 
     def __contains__(self, item):
@@ -75,8 +60,10 @@ class Map:
     def getItems(self):
         return self.dataType1.items()
 
-    def clear(self):
-        self.dataType1 = None
-
     def search(self, key):
+
         return True
+
+    def __getitem__(self, key):
+        self.search(key)
+        return key
