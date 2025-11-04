2cd5796ed8cefd06bbe67c1dc2551bfe0331be73
OliverG776 <olivergrushka475@gmail.com>
2025-11-03 20:53:20 -0500

    -Genre sort now implemented properly on UserInterface.py

--- UserInterface.py
+++ UserInterface.py
@@ -7,12 +7,24 @@ import streamlit as st
 #Variables:
 
 #Defines columns, essentially partitions the page so we can set assets to certain parts of the page
+showSidebarNavigation = False
 col1, col2, col3 = st.columns([1, 3, 1])
 #Sets user's default search to ""
 if 'search_key' not in st.session_state:
     st.session_state['search_key'] = ""
 
 if 'searchGenre' not in st.session_state:
+    st.session_state['searchGenre'] = False
+
+# Allows the user to choose what category they want to sort by
+search_toggle = st.radio(
+    "Search by:", ("Film Title", "Genre"), horizontal=True
+)
+
+# #Switches what user is searching by
+if search_toggle == "Genre":
+    st.session_state['searchGenre'] = True
+else:
     st.session_state['searchGenre'] = False
 
 #UI Setup
@@ -30,20 +42,10 @@ if st.session_state['searchGenre'] == False:
 else:
     search = st.selectbox(
         "Which genre?",
-        ("Email", "Home phone", "Mobile phone"),
+        ("Action", "Adventure", "Animation", "Children's", "Comedy", "Crime", "Documentary", "Drama", "Fantasy", "Film-Noir", "Horror", "Musical", "Mystery",
+         "Romance", "Sci-Fi", "Thriller", "War", "Western", "No genre")
     )
 
-
-# Allows the user to choose what category they want to sort by
-search_toggle = st.radio(
-    "Search by:", ("Film Title", "Genre"), horizontal=True
-)
-
-# #Switches what user is searching by
-if search_toggle == "Genre":
-    st.session_state['searchGenre'] = False
-else:
-    st.session_state['searchGenre'] = True
 
 #Top films page
 with col3:
