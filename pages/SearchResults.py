48ef0b2fe261ec8f50023047c9cabc83a7b8c5b6
niveK143 <KevinJosh5339@gmail.com>
2025-11-04 14:43:17 -0500

    Fixed search parantheses' check for movie year now using regex

--- pages/SearchResults.py
+++ pages/SearchResults.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import os
 import csv
+import re
 
 #Initializing streamlit variables
 col1, col2, col3 = st.columns([1, 3, 1])
@@ -20,15 +21,22 @@ with open(file_directory, newline="", encoding="utf-8") as csvfile:
     for row in reader:
         # temp = row[1].split("(")
         # if temp[1][0].isnumeric():
-        title = row[1].split("(")
-        title[0] = title[0][:-1]
+        title = row[1]
+        #title = row[1].split("(")
+        #title[0] = title[0][:-1]
+        match = re.match(r"(.*)\s\((\d{4})\)$",title)
+        if match:
+            movie_title, movie_year = match.groups()
+        else:
+            movie_title = title
+            movie_year = "Year not found"
         # else:
         #     title = row[1]
         #     lastParenthesis = title.rfind("(")
         #     title = title[:lastParenthesis]
 
-        if title[0].lower() == st.session_state['search_key'].lower():
-            st.write(title[0])
+        if movie_title.lower() == st.session_state['search_key'].lower():
+            st.write(movie_title + " - " + movie_year)
             st.write("Genres: " + row[2])
             #Need to check map here to pull out ratings/potentially switch genres to come from map
 
