8b2c5f5575c38eff617a67880fe67aac6786fa0d
OliverG776 <olivergrushka475@gmail.com>
2025-11-04 13:32:05 -0500

    -Updated DataInitialization w/ testing features/comments

--- DataInitialization.py
+++ DataInitialization.py
@@ -13,13 +13,18 @@ movieMap = Map()
 #This is just for testing, this code is legit super slow bc i just wanna test that I can actually mess w/ the map lmao
 #Kevin if you see this, please make this faster or we will get a fat 0
 
+#Opens movies file
 with open(movies_directory, newline="", encoding="utf-8") as csvfile:
+
     reader = csv.reader(csvfile)
     next(reader)
     i = 1
     for row in reader:
-        print("Number: " + str(i))
-        print("Film ID: " + str(row[0]))
+
+        #This is used for testing purposes
+
+        #print("Number: " + str(i))
+        #print("Film ID: " + str(row[0]))
         i+=1
         #print(row[0])
         #print(row[1])
@@ -29,19 +34,33 @@ with open(movies_directory, newline="", encoding="utf-8") as csvfile:
         totalReviews = 0.0
         scoreTotal = 0.0
 
+        #Opens the ratings file for the specific film
+
         with open(ratings_directory, newline="", encoding="utf-8") as csvfile:
             reader = csv.reader(csvfile)
             next(reader)
 
+            #Used for tallying score but is wildly inefficient, just used for testing
 
             for row in reader:
                 if currentID == row[0]:
                     scoreTotal += float(row[2])
                     totalReviews += 1
-        print("scoreTotal: " + str(scoreTotal))
-        print("totalReviews: " + str(totalReviews))
-        if(totalReviews != 0):
-            print("Net Review: " + str(scoreTotal/totalReviews) + "\n")
+        #print("scoreTotal: " + str(scoreTotal))
+        #print("totalReviews: " + str(totalReviews))
+        # if(totalReviews != 0):
+        #     #print("Net Review: " + str(scoreTotal/totalReviews) + "\n")
 
-        if(totalReviews != 0):
-            movieMap.insert(row[1], row[2], scoreTotal/totalReviews)
+    #Splitting title up from year, idk if this actually works but it did for my SearchResults
+    title = row[1].split("(")
+    title[0] = title[0][:-1]
+
+
+
+    #Some films have no reviews for some reason so if the total reviews = 0 then we dont wanna divide by 0
+    if(totalReviews != 0):
+        movieMap.insert(title, row[2], scoreTotal/totalReviews)
+    else:
+        movieMap.insert(title, row[2], 0)
+
+    print("Title: " + str(title) + " Genres: " + str(row[2]))
