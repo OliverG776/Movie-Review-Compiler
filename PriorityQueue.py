da1a12990f4a1931208bc250a8c266a026e0f9a4
parkercolby <colbyparker1905@gmail.com>
2025-11-04 23:04:21 -0500

    Priority Queue (Finished?)
    The TopTen Function has a major issue regarding keeping the order of the heap. Maybe fix by reinserting after getting it?
    
    Needs Testing
    
    Needs Documentation

--- PriorityQueue.py
+++ PriorityQueue.py
@@ -134,37 +134,75 @@ class PriorityQueue:
         self.heapContainer = [None] * 10
         self.size = 0
 
-    def getParent(self):
-        print("test")
 
-    def getLeftChild(self, index):
-        if 2*index+1 < 0 or 2*index+1 > self.size:
-            return None
-        else:
-            return self.heapContainer[2*index+1]
+    #Was Really Redundant Using These, Cleaner Without Them, Kept in Case You Need Them For UI
+    # def getParent(self, index):
+    #     if (int)((index-1)/2) < 0:
+    #         return None
+    #     else:
+    #         return self.heapContainer[(int)((index-1)/2)]
+    #
+    # def getLeftChild(self, index):
+    #     if 2*index+1 >= self.size:
+    #         return None
+    #     else:
+    #         return self.heapContainer[2*index+1]
+    #
+    # def getRightChild(self, index):
+    #     if 2*index+2 >= self.size:
+    #         return None
+    #     else:
+    #         return self.heapContainer[2*index+2]
 
-    def getRightChild(self, index):
-        if 2*index+2 < 0 or 2*index+2 > self.size:
-            return None
-        else:
-            return self.heapContainer[2*index+2]
+    def resize(self):
+        oldContainer = self.heapContainer
+        newSize = len(self.heapContainer)*2
+        self.heapContainer = [None]*newSize
+        for i in range(len(oldContainer)):
+            self.heapContainer[i] = oldContainer[i]
+
 
     def insert(self, title, rating):
+        if self.size == len(self.heapContainer):
+            self.resize()
         self.heapContainer[self.size] = (title, rating)
         self.size += 1
         self.heapifyUp()
 
-
     def heapifyUp(self):
-        return 0
+        index  = self.size-1
+        while index > 0:
+            parentIndex = (int)((index-1)/2)
+            if self.heapContainer[index][1]>self.heapContainer[parentIndex][1]:
+                temp = self.heapContainer[index]
+                self.heapContainer[index] = self.heapContainer[parentIndex]
+                self.heapContainer[parentIndex] = temp
+                index = parentIndex
+            else:
+                break
 
-    def heapifyDown(self):
-        return 0
+    def heapifyDown(self, index):
+        leftIndex = 2*index+1
+        rightIndex = 2*index+2
+        largestIndex = index
+        if leftIndex < self.size and  self.heapContainer[leftIndex][1]>self.heapContainer[largestIndex][1]:
+            largestIndex = leftIndex
+        if rightIndex < self.size and self.heapContainer[rightIndex][1]>self.heapContainer[largestIndex][1]:
+            largestIndex = rightIndex
+
+        if largestIndex != index:
+            temp = self.heapContainer[index]
+            self.heapContainer[index] = self.heapContainer[largestIndex]
+            self.heapContainer[largestIndex] = temp
+            self.heapifyDown(largestIndex)
 
     def pop(self):
-        self.heapContainer[0] = self.heapContainer[self.size]
+        if self.size == 0:
+            return None
+        self.heapContainer[0] = self.heapContainer[self.size-1]
+        self.heapContainer[self.size-1] = None
         self.size -= 1
-        self.heapifyDown()
+        self.heapifyDown(0)
 
     def top(self):
         return self.heapContainer[0]
