a2c79850fac4e1d12ab3009e676f0d2b814ebf8c
niveK143 <KevinJosh5339@gmail.com>
2025-11-07 07:18:16 -0500

    Removed redundant code in SearchResults.py and cleaned some unnecessary comments

--- pages/SearchResults.py
+++ pages/SearchResults.py
@@ -6,11 +6,19 @@ import csv
 import re
 
 #Initializing streamlit variables
-col1, col2, col3 = st.columns([1, 5, 1])
+col1, col2, col3 = st.columns([1, 3, 1])
 script_directory = os.path.dirname(__file__)
 file_directory = os.path.join(script_directory,"movies.csv")
 
+# Initialize Data into movieMap (contains all movies with their information)
 movieMap = initializeData()
+
+
+# check for genreSearchCheck 
+# if true search_key will be genre type
+# else search_key will be title
+genreSearchCheck = st.session_state['searchGenre']
+print(str(genreSearchCheck))
 
 if 'search_key' not in st.session_state:
     st.session_state['search_key'] = ""
@@ -20,6 +28,7 @@ searchedMovie = st.session_state['search_key']
 
 if 'releaseYear' not in st.session_state:
     st.session_state['releaseYear'] = "N/A"
+
 
 with col2:
     st.title("Search Results for " + searchedMovie + ":")
@@ -38,39 +47,9 @@ if movieItems != False and st.session_state['searchGenre'] == False:
             print("Clicked a movie, redirect to card")
 
             st.switch_page("pages/MovieCard.py")
-
-
-    with open(file_directory, newline="", encoding="utf-8") as csvfile:
-        reader = csv.reader(csvfile)
-        next(reader)
-        i = 1
-        title = ""
-        for row in reader:
-            # temp = row[1].split("(")
-            # if temp[1][0].isnumeric():
-            title = row[1]
-            #title = row[1].split("(")
-            #title[0] = title[0][:-1]
-            match = re.match(r"(.*)\s\((\d{4})\)$",title)
-            if match:
-                movie_title, movie_year = match.groups()
-                #print(movie_title + " - " + str(movie_year))
-            else:
-                movie_title = title
-                movie_year = "Year not found"
-                print("Movie Title not found")
-            # else:
-            #     title = row[1]
-            #     lastParenthesis = title.rfind("(")
-            #     title = title[:lastParenthesis]
-
-            if movie_title.lower() == st.session_state['search_key'].lower():
-                st.session_state['releaseYear'] = movie_year
-            #     st.write("Release Year: " + movie_year)
-            #     st.write("Genres: " + row[2])
-                #Need to check map here to pull out ratings/potentially switch genres to come from map
 else:
     st.write("No search results found")
+
 
     #ISSUE: Film year included as well, we can probably fix it just by identifying when we hit the first (
 
