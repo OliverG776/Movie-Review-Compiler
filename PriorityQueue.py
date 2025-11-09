dd4f3e64c0299aec53defa464443198545b7d144
parkercolby <colbyparker1905@gmail.com>
2025-11-08 20:19:54 -0500

    Comments Added

--- PriorityQueue.py
+++ PriorityQueue.py
@@ -2,10 +2,22 @@
 #Implement a list of the top 100 films, which includes the film's ranking, film title, release year, and overall rating score
 
 class PriorityQueue:
-    def __init__(self):
-        self.heapContainer = [None] * 10
-        self.size = 0
+    # A Max-Heap–based priority queue for storing movies
+    # by their rating score.
 
+    # Each element is a tuple:
+    #     (title, rating)
+
+    # The highest-rated movie is always at the top of the heap
+    
+    def __init__(self):
+        #initializes an array with 10 elements to start
+        self.heapContainer = [None] * 10
+        self.size = 0 #Number of elements currently in the heap
+
+    # Doubles the heap's capacity when full.
+    # Copies all existing elements into the new larger container.
+    
     def resize(self):
         oldContainer = self.heapContainer
         newSize = len(self.heapContainer)*2
@@ -13,14 +25,17 @@ class PriorityQueue:
         for i in range(len(oldContainer)):
             self.heapContainer[i] = oldContainer[i]
 
-
+    # Inserts a new (title, rating) pair into the heap.
+    #  If the heap is full, it resizes automatically.
     def insert(self, title, rating):
         if self.size == len(self.heapContainer):
             self.resize()
         self.heapContainer[self.size] = (title, rating)
         self.size += 1
         self.heapifyUp()
-
+        
+   # Restores the heap property after insertion by moving the
+   # newly added node upward as long as it's larger than its parent
     def heapifyUp(self):
         index  = self.size-1
         while index > 0:
@@ -33,6 +48,8 @@ class PriorityQueue:
             else:
                 break
 
+    # Restores the heap property after removing the top element
+    # by moving the element at 'index' down until it is in the correct position.
     def heapifyDown(self, index):
         leftIndex = 2*index+1
         rightIndex = 2*index+2
@@ -48,6 +65,9 @@ class PriorityQueue:
             self.heapContainer[largestIndex] = temp
             self.heapifyDown(largestIndex)
 
+    
+    # Replaces the root with the last element, decreases size,
+    #  and restores the heap property
     def pop(self):
         if self.size == 0:
             return None
@@ -56,9 +76,12 @@ class PriorityQueue:
         self.size -= 1
         self.heapifyDown(0)
 
+    #  Returns the movie with the highest rating without removing it
     def top(self):
         return self.heapContainer[0]
 
+
+    #Retrieves the top 1000 movies from the heap in order of rating
     def getTopThousand(self):
         topThousand = []
         for i in range(1000):
